f502d7d84e5724290da4de37e93253c4809c0333
yevgeniykonovalov <zhosakic@gmail.com>
2020-04-03 14:31:44 +0600

    feat: add files for work

---
+++ src/KeyArray.js
@@ -0,0 +1,221 @@
+const keyArr = {
+    Backquote : {
+        en : '§',
+        ru : 'ё',
+        shiftEn : '±',
+        shiftRu : 'Ё',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    Digit1 : {
+        en : '1',
+        ru : '1',
+        shiftEn : '!',
+        shiftRu : '!',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    Digit2 : {
+        en : '2',
+        ru : '2',
+        shiftEn : '@',
+        shiftRu : '"',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    Digit3 : {
+        en : '3',
+        ru : '3',
+        shiftEn : '#',
+        shiftRu : '№',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    Digit4 : {
+        en : '4',
+        ru : '4',
+        shiftEn : '$',
+        shiftRu : ';',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    Digit5 : {
+        en : '5',
+        ru : '5',
+        shiftEn : '%',
+        shiftRu : '%',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    Digit6 : {
+        en : '6',
+        ru : '6',
+        shiftEn : '^',
+        shiftRu : ':',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    Digit7 : {
+        en : '7',
+        ru : '7',
+        shiftEn : '&',
+        shiftRu : '?',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    Digit8 : {
+        en : '8',
+        ru : '8',
+        shiftEn : '*',
+        shiftRu : '*',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    Digit9 : {
+        en : '9',
+        ru : '9',
+        shiftEn : '(',
+        shiftRu : '(',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    Digit0 : {
+        en : '0',
+        ru : '0',
+        shiftEn : ')',
+        shiftRu : ')',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    Minus: {
+        en : '-',
+        ru : '-',
+        shiftEn : '_',
+        shiftRu : '_',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    Equal: {
+        en : '=',
+        ru : '=',
+        shiftEn : '+',
+        shiftRu : '+',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    Backspace: {
+        en : '<-',
+        ru : '<-',
+        shiftEn : '<-',
+        shiftRu : '<-',
+        keyType : ['key', 'functional', 'backspace'],
+        Action : ['functional']
+    },
+    Tab: {
+        en : 'Tab',
+        ru : 'Tab',
+        shiftEn : 'Tab',
+        shiftRu : 'Tab',
+        keyType : ['key', 'functional', 'backspace'],
+        Action : ['functional']
+    },
+    KeyQ: {
+        en : 'q',
+        ru : 'й',
+        shiftEn : 'Q',
+        shiftRu : 'Й',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    KeyW: {
+        en : 'w',
+        ru : 'ц',
+        shiftEn : 'W',
+        shiftRu : 'Ц',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    KeyE: {
+        en : 'e',
+        ru : 'у',
+        shiftEn : 'E',
+        shiftRu : 'У',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    KeyR: {
+        en : 'r',
+        ru : 'к',
+        shiftEn : 'R',
+        shiftRu : 'К',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    KeyT: {
+        en : 't',
+        ru : 'е',
+        shiftEn : 'T',
+        shiftRu : 'Е',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    KeyY: {
+        en : 'y',
+        ru : 'н',
+        shiftEn : 'Y',
+        shiftRu : 'Н',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    KeyU: {
+        en : 'u',
+        ru : 'г',
+        shiftEn : 'U',
+        shiftRu : 'Г',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    KeyI: {
+        en : 'i',
+        ru : 'ш',
+        shiftEn : 'I',
+        shiftRu : 'Ш',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    KeyO: {
+        en : 'o',
+        ru : 'щ',
+        shiftEn : 'O',
+        shiftRu : 'Щ',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    KeyP: {
+        en : 'p',
+        ru : 'з',
+        shiftEn : 'P',
+        shiftRu : 'З',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    BracketLeft: {
+        en : '[',
+        ru : 'х',
+        shiftEn : '{',
+        shiftRu : 'Х',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+    BracketRight: {
+        en : ']',
+        ru : 'ъ',
+        shiftEn : '}',
+        shiftRu : 'Ъ',
+        keyType : ['key'],
+        Action : ['key-symbol']
+    },
+
+};
+
+export { keyArr };
---
+++ src/Variables.js
@@ -0,0 +1,5 @@
+let currentStateLang = 'ru';
+
+
+
+export { currentStateLang };
